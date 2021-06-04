ba6190409f9d1129e60668d552361fd29516dc17
giannoul <giannoul88@gmail.com>
2021-06-04 14:04:47 +0300

    Limit the /metrics route to prometheus by chekcing the request's IP and User Agent

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'prometheus/middleware/collector'
+require 'prometheus/middleware/exporter'
+require_relative '../lib/middlewares/metrics_route_filter'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +13,9 @@ module PrometheusMetrics
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.middleware.use MetricsRouteFilter
+    config.middleware.use Prometheus::Middleware::Collector
+    config.middleware.use Prometheus::Middleware::Exporter
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
---
+++ lib/middlewares/metrics_route_filter.rb
@@ -0,0 +1,37 @@
+class MetricsRouteFilter
+    def initialize app
+      @app = app
+    end
+  
+    def call env
+      @status, @headers, @response = @app.call(env)
+  
+      Rails.logger.debug "=" * 50
+      Rails.logger.debug "Request IP: #{env['REMOTE_ADDR']} "
+      Rails.logger.debug "Request Path: #{env['REQUEST_PATH']} "
+      Rails.logger.debug "Request UserAgent: #{env['HTTP_USER_AGENT']} "
+      
+      if env['REQUEST_PATH'] == "/metrics"
+        if is_prometheus(env['REMOTE_ADDR'], env['HTTP_USER_AGENT'])
+          Rails.logger.debug "Proceed with request"
+          Rails.logger.debug "=" * 50
+          [@status, @headers, @response]
+        else
+          Rails.logger.debug "Block request"
+          Rails.logger.debug "=" * 50
+          forbidden
+        end
+      end
+
+      
+    end
+
+    def forbidden
+      [403, {"Content-Type" => "text/plain"}, ["Forbidden!"]]
+    end
+
+    def is_prometheus(ip, user_agent)
+      (ip.split(".")[0] == "10") && (user_agent.include? "Prometheus")
+    end
+
+  end
